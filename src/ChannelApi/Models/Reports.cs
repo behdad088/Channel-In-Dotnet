a867943d64f8c741d2a40baba700ee5847f5dc16
Behdad Kardgar <behdad.kardgar1@gmail.com>
2024-09-17 17:29:30 +0200

    small refactoring

--- src/ChannelApi/Models/Reports.cs
+++ src/ChannelApi/Models/Reports.cs
@@ -1,15 +1,7 @@
 ﻿namespace ChannelApi.Models
 {
-    public record ReportModel
+    public record ReportModel(object Event, string EventName)
     {
-        public object Event { get; init; }
-        public string EventName { get; init; }
         public DateTime EventDate { get; init; } = DateTime.Now;
-
-        public ReportModel(object @event, string eventName)
-        {
-            Event = @event;
-            EventName = eventName;
-        }
     }
 }
